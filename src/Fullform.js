c6dff803ca4ca4a03505e9ee9a55068f34cd5d65
kavya163 <kavyaayyappan1990@gmail.com>
2021-08-13 09:16:46 -0700

    Adding all field validations

--- src/Fullform.js
+++ src/Fullform.js
@@ -9,11 +9,12 @@ class Form extends Component {
     email: "",
     password: "",
     formError: { firstname: "", lastname: "", email: "", password: "" },
-    firstNameValid: false,
-    lastNameValid: false,
-    passwordValid: false,
-    emailValid: false,
-    formValid: false,
+    firstNameValid: true,
+    lastNameValid: true,
+    passwordValid: true,
+    emailValid: true,
+    formValid: true,
+    formSubmission: false,
   };
 
   // handlingOnChange sets the current state to the state objects(state.username , state.password,..)
@@ -42,7 +43,7 @@ class Form extends Component {
         console.log(b);
         break;
       case "lastname":
-        lastNameValid = b.length >0;
+        lastNameValid = b.length > 0;
         formError.lastname = lastNameValid
           ? ""
           : "Lastname should be filled out";
@@ -62,33 +63,55 @@ class Form extends Component {
       default:
         break;
     }
-    this.setState(
-      {
-        formError: formError,
-        firstNameValid: firstNameValid,
-        lastNameValid: lastNameValid,
-        passwordValid: passwordValid,
-        emailValid: emailValid,
-      },
-      this.fullValidation
-    );
   }
-
-  //validating full form
-  // fullValidation() {
-  //   this.setState({
-  //     formValid:
-  //       this.state.FirstNameValid &&
-  //       this.state.LastNameValid &&
-  //       this.state.passwordValid &&
-  //       this.state.emailValid,
-  //   });
-  // }
-
 
   alertmsg = () => {
     alert("Thank you!! Your information has been submitted.");
   };
+
+  handleButtonClicked() {
+    var fname = true;
+    var lname = true;
+    var eError = true;
+    var pError = true;
+
+    var fSubmit = this.state.formSubmission;
+
+    if (this.state.lastname.length <= 0) {
+      console.log("Inside Last Name");
+      lname = false;
+      fSubmit = false;
+    }
+    if (this.state.firstname.length <= 0) {
+      console.log("Inside First Name");
+      fname = false;
+      fSubmit = false;
+    }
+    if (!this.state.email.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i)) {
+      eError = false;
+      fSubmit = false;
+    }
+    if (
+      !this.state.password.match("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,})")
+    ) {
+      pError = false;
+      fSubmit = false;
+    }
+
+    this.setState({
+      firstNameValid: fname,
+      lastNameValid: lname,
+      emailValid: eError,
+      passwordValid: pError,
+      formError: {
+        firstname: fname ? "" : "Please enter valid First Name",
+        lastname: lname ? "" : "Please enter valid Last Name",
+        email: eError ? "" : "Please check your Email",
+        password: pError ? "" : "Please check your Password",
+      },
+      formSubmission: fSubmit,
+    });
+  }
 
   //render function returns a form where all the fields like email, password, username contain
   render() {
@@ -107,7 +130,7 @@ class Form extends Component {
             //reuired field
             required
           />
-          {this.state.formError.firstname && (
+          {this.state.firstNameValid ? null : (
             <p>{this.state.formError.firstname}</p>
           )}
 
@@ -115,14 +138,14 @@ class Form extends Component {
           <input
             type="text"
             name="lastname"
-            placeholder="enter your lastname"
+            placeholder="Enter your last name"
             value={this.state.lastname}
             //when the boxfield gets triggered it calls the handlingOnchange
             onChange={this.handlingOnChange}
             //reuired field
             required
           />
-          {this.state.formError.lastname && (
+          {this.state.lastNameValid ? null : (
             <p>{this.state.formError.lastname}</p>
           )}
 
@@ -158,7 +181,11 @@ class Form extends Component {
           )}
           <br></br>
           {/* submit button is disabled until all the form field validation elements are true. */}
-          <button type="submit" >
+          <button
+            type="submit"
+            onClick={this.handleButtonClicked.bind(this)}
+            disabled={this.state.formSubmission}
+          >
             Submit
           </button>
         </form>
